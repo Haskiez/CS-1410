5d3903afa399f29d656bc133dc6fce7bf5dfc7a9
Braden <haskiezgaming@gmail.com>
2016-12-01 18:41:09 -0700

    Create Exceptions.h

---
+++ Lab05/Exceptions.h
@@ -0,0 +1,25 @@
+// Course: CS 1410
+// Instructor: Professor Wagstaff
+// File: Exceptions.h
+// Contents: Lab #05 Programming Assignment
+// I declare that the following source code was written
+// solely by me, or provided by the instructor. 
+// I understand that copying any source
+// code, in whole or in part, constitutes cheating, and
+// that I will receive a zero in this assignment if I am
+// found in violation of this policy.
+// ---------------------------------------------------------
+
+#pragma once
+#include <iostream>
+using namespace std;
+class Exceptions
+{
+private:
+	int error;
+public:
+	Exceptions();
+	Exceptions(int);
+	void getError();
+	~Exceptions();
+};
