4d9ed2f6a3a8ea601a120e59dc3d108fc7370131
Braden <haskiezgaming@gmail.com>
2016-12-01 19:25:45 -0700

    Create driver.h

---
+++ Project06/driver.h
@@ -0,0 +1,19 @@
+// Course: CS 1410
+// Instructor: Professor Wagstaff
+// File: project06.cpp
+// Contents: Project #6 Programming Assignment
+// I declare that the following source code was written
+// solely by me, or provided by the instructor. 
+// I understand that copying any source
+// code, in whole or in part, constitutes cheating, and
+// that I will receive a zero in this assignment if I am
+// found in violation of this policy.
+// -----------------------------------------------------------
+
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+int index_of(const string& s, const string& t);
+int recursiveFind(const string& s, const string& t, int index);
